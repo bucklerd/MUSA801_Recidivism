b229365336e1be7cd3140c382b1e775c8ab137ef
bucklerd <bucklerd@upenn.edu>
2019-05-14 00:46:12 -0400

    Updated for final project

--- js/main.js
+++ js/main.js
@@ -5,8 +5,12 @@ var mainData = data;
 var accepted = [];
 var bdgt = 0;
 var tempRec;
+var tempData;
+var map;
+var recid;
+var cost;
 
-
+//Define the table function to build the main ex-offender data table
 var table = () => { $('#exOffenderTable').DataTable({
   data: mainData,
   order: [[2, "desc"]],
@@ -27,11 +31,15 @@ var table = () => { $('#exOffenderTable').DataTable({
   searching: false,
   info: false
 });
+
+//Define button functionality for selecting details of an ex-offender row
 $('#exOffenderTable tbody').on( 'click', '.disp', function () {
   oneRecord = $('#exOffenderTable').DataTable().row( $(this).parents('tr') ).data();
   buildDetail();
   $('#dtlClose').show()
 } );
+
+//Define checkbox functionality for selecting ex-offender for program list
 $('#exOffenderTable tbody').on( 'click', '.tally', function () {
    tempRec = $('#exOffenderTable').DataTable().row( $(this).parents('tr') ).data();
   if($(this).is(":checked")) {
@@ -61,26 +69,81 @@ $('#updateTable').on('click', function(){
   });
   $('#exOffenderTable').DataTable().destroy();
   table();
+  accepted=[];
 });
 };
 
-
+//When document is ready, run final setup funcitons.
 $(document).ready(function() {
-  $('#inmateDtls').hide();
-  $('#dtlClose').hide();
-  //$('#selectedTable').hide();
-  table();
+  $('#inmateDtls').hide(); //hide detail  table initially
+  $('#dtlClose').hide(); //hide close button
+  $('#map').hide(); //hide map in about button
+  table(); //render main ex-offender datatable
+  //On recidivism ratio button click, display map, renderbasemap, ajax call for data, render polygons on map
   $('#recidRatopBtn').on('click', function(){
     console.log("recidRatopBtn");
 
+    $.ajax("https://gist.githubusercontent.com/bucklerd/d453630f398531998f5c2b4a9db00dde/raw/1c7baf67dee2b14eb712104f0639ada14978e6ea/recidRatio.geojson").done(function(dat){
+      console.log("Successful Load recidRatio");
+      $('#map').show();
+      if(recid){ map.removeLayer(recid); }
+      if(cost){ map.removeLayer(cost); }
+      if(!map) {
+        map = L.map('map', {
+          center: [36.086818, -79.797872],
+          zoom: 10
+        })
+        var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+        }).addTo(map);
+      }
+
+      var tempData=JSON.parse(dat);
+      recid = L.geoJson(tempData, {style:function(feature){
+        return {fillColor: getColor2(feature.properties.recidRatio),
+                weight: 1,
+                color: 'grey',
+                fillOpacity: 0.8
+              }
+      }}).addTo(map);
+    });
   });
+
+  //On costtracks button click, display map, renderbasemap, ajax call for data, render polygons on map
   $('#costTracts').on('click', function(){
     console.log("costTracts");
 
+    $.ajax("https://gist.githubusercontent.com/bucklerd/45f0c0f5b735d20c8781ba50510bd762/raw/586c8541178e72586d555c8bb3cb5e7e03bfe496/costByTracts.geojson").done(function(dat){
+      console.log("Successful Load costTracts")
+      $('#map').show();
+      if(recid){ map.removeLayer(recid); }
+      if(cost){ map.removeLayer(cost); }
+
+      if(!map) {
+        map = L.map('map', {
+          center: [36.086818, -79.797872],
+          zoom: 10
+        })
+        var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+        }).addTo(map);
+      }
+
+      tempData=JSON.parse(dat);
+      cost = L.geoJson(tempData, {style:function(feature){
+        return {fillColor: getColor(feature.properties.tractCost),
+                weight: 1,
+                color: 'grey',
+                fillOpacity: 0.8
+              }
+      }}).addTo(map);
+    });
   });
+});
 
-} );
-
+//Build/rebuild datatable of selected ex-offenders
 var selected = () => {
   $('#selectedTable').DataTable({
     data: accepted,
@@ -104,21 +167,43 @@ var fields = ["name", "releaseDate", "race", "ethnicity", "sex", "age", "mostSer
 "Cnt_Felony_Tot",  "totalSentenceCount", "juvnileOffensesFlag", "days_served_in_doc_custody",
 "totalDisciplineInfractions"];
 
-
+//builds the ex-offender details page
 var buildDetail = () => {
   $('#description').hide();
   $('#inmateDtls').hide();
   _.each(fields, function(field){
-    $('#' + field).val(eval('oneRecord.' + field));
+    $('#' + field).val(eval('oneRecord.' + field.toLower()));
   });
   $('#inmateDtls').show();
 };
 
-
+//updates tracker stats at bottom of page
 var statUpdate = () => {
   runningTotal = progCost * accepted.length;
   $('#costTotal').val(runningTotal);
   var toSpend = bdgt - runningTotal;
   $('#toSpend').val(toSpend);
 
+}
+
+//helper for map renders in About Page
+//sets bins for cost-by-tract map
+function getColor(d) {
+    return d < 150000 ? '#dddddd' :
+           d < 350000  ? '#b1a4c7' :
+           d < 550000  ? '#846cb0' :
+           d < 850000  ? '#543898' :
+           d < 2300000   ? '#000080' :
+                      '#808080';
+}
+
+//helper for map renders in About Page
+//sets bins for recidivism ratio map
+function getColor2(d) {
+    return d < .75 ? '#00c6bb ' :
+           d < 1.0  ? '#84b094 ' :
+           d < 1.4  ? '#ae996d ' :
+           d < 1.75  ? '#ca7f46 ' :
+           d < 2.1   ? '#de611c' :
+                      '#808080';
 }
